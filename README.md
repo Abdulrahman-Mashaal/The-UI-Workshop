98afca3a8fcf9efde0bdef788e9abf41cec4a45f
Abdulrahman Mashaal <abdul.attiamashaal@gmail.com>
2025-02-17 08:47:17 +0200

    anma services

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # The-UI-Workshop
 FrontEnd-Projects
 ## Projects
+
+## Command lines
+### Sass watch examples
+```bash
+sass slider/futureface/clients/assets/scss/style.scss slider/futureface/clients/assets/styles/style.css --watch
+sass slider/anma/specialities/assets/scss/style.scss slider/anma/specialities/assets/styles/style.css --watch
+sass slider/anma/services/assets/scss/style.scss slider/anma/services/assets/styles/style.css --watch
+```
---
+++ slider/anma/services/assets/scripts/app.js
@@ -0,0 +1,38 @@
+const anmaServicesSlider = new Swiper('.services', {
+    slidesPerView: 1,
+    spaceBetween: 32,
+    navigation: {
+        nextEl: ".services__swiper-button-next",
+        prevEl: ".services__swiper-button-prev",
+      },
+    // loop: true,
+    breakpoints:{
+        1200: {
+            slidesPerView: 4,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+        992: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        576: {
+            slidesPerView: 1,
+        },
+        430: {
+            slidesPerView: 1,
+        }
+
+    },
+    pagination: {
+        el: ".services__swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2000
+    },
+
+});
